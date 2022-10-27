f365fbc1c2637d04dafaf3e9721a551a6c3e05c9
ZeroPointNothing <116678675+ZeroPointNothing@users.noreply.github.com>
2022-10-26 22:08:23 -0400

    Update phs1.bat

--- GAME/phs1.bat
+++ GAME/phs1.bat
@@ -1,11 +1,26 @@
 @echo off
 
+
+
+if exist data.loc goto start
+if not exist data.loc goto noStUp
+
+:start
 title BatchQuest
 echo BatchQuest is best played in fullscreen mode.
 echo.
 pause
 cls
 
+goto menu
+:noStUp
+echo BatchQuest requires files that you may not have. Please make sure to run BatchQuest.bat to safely start the game.
+echo.
+pause
+exit
+
+
+:menu
 echo Welcome To . . .
 timeout 2 /nobreak >nul
 
