33e4b36c88b1e646d61a3a8285482880296309f4
ZeroPointNothing <116678675+ZeroPointNothing@users.noreply.github.com>
2022-10-29 12:52:06 -0400

    Updated MainScript
    - Began work on options.
    - New Game option is ready for the story
    - Settings now has a way to get to the GitHub

--- GAME/DATA/phs1.bat
+++ GAME/DATA/phs1.bat
@@ -10,7 +10,6 @@ title BatchQuest
 echo BatchQuest is best played in fullscreen mode.
 echo.
 pause
-cls
 
 goto menu
 :noStUp
@@ -21,8 +20,10 @@ exit
 
 
 :menu
+cls
 echo Welcome To . . .
 timeout 2 /nobreak >nul
+
 
 echo       :::::::::      ::: ::::::::::: ::::::::  :::    :::                               ::::::::   :::    ::: :::::::::: :::::::: ::::::::::: 
 echo      :+:    :+:   :+: :+:   :+:    :+:    :+: :+:    :+:                              :+:    :+:  :+:    :+: :+:       :+:    :+:    :+:      
@@ -32,5 +33,66 @@ echo   +#+    +#+ +#+     +#+ +#+    +#+        +#+    +#+                      
 echo  #+#    #+# #+#     #+# #+#    #+#    #+# #+#    #+#                              #+#    #+#  #+#    #+# #+#       #+#    #+#    #+#          
 echo #########  ###     ### ###     ########  ###    ###                               ########### ########  ########## ########     ###           
 echo.
+echo.
+
+:mOpt
+echo 1- New Game
+echo 2- Load Game
+echo 3- Settings
+echo 4- Exit
+echo.
+set /p mChs=
+
+if '%mChs%'=='1' goto nwGame
+if '%mChs%'=='2' goto ldGame
+if '%mChs%'=='3' goto sett
+if '%mChs%'=='4' goto exit
+
+:: sends if user gives an invalid response
+echo.
+echo %mChs% is not a valid option!
+timeout 1 /nobreak >nul
+goto mOpt
+
+:ldGame
+echo.
+echo This feature has not been developed yet. . .
+pause
+goto menu
+
+:sett
+cls
+echo --------- SETTINGS ---------
+echo 1. Go to BQ GitHub
+echo 2. Reset BQ (Deletes all non-starting data!)
+echo.
+set /p setAns=
+
+if '%setAns%'=='1' goto GitHub
+if '%setAns%'=='2' exit
+
+cls
+goto sett
+
+
+:nwGame
+echo.
+echo Are you sure? Starting a new game will erase any existing save data.
+set /p nwgAns=
+
+if '%nwgAns%'=='y' goto gameStart
+if '%nwgAns%'=='n' goto menu
+
+:exit
+exit
+
+:GitHub
+start https://github.com/ZeroPointNothing/BatchQuest
+goto menu
+
+
+:gameStart
+cls
+echo There's nothing here yet...
 pause
 exit
